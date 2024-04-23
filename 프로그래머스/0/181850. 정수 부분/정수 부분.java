60cff7af55043ded7d528f22dca63af972820c47
hozn_e <hwlsdl37@gmail.com>
2024-04-23 22:59:21 +0900

    [level 0] Title: 정수 부분, Time: 0.01 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(double flo) {
+        // 정답 선언 및 초기화
+        int answer = 0;
+        // flo를 int로 형변환
+        answer = (int)flo;
+
+        // 정답 리턴
+        return answer;
+    }
+}
