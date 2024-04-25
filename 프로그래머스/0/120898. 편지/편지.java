17ae3876bb15df9eb121503b155dd734f84d6224
hozn_e <hwlsdl37@gmail.com>
2024-04-25 21:29:21 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        for (int i = 0; i < message.length(); i++) {
+            answer = message.length();
+        }
+        return answer * 2;
+    }
+}
