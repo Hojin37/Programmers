ad2d945d8c7c807a636968a91c6d5b89c95348d0
hozn_e <hwlsdl37@gmail.com>
2024-04-29 22:36:48 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -3,11 +3,11 @@ class Solution {
         // 정답 선언 및 초기화
         String answer = "";
         // a를 우선 숫자 0으로 변경 
-        String str1 = myString.replace("a", "-");
+        String str1 = myString.replace("a", "0");
         // 전체를 소문자로 변환
         String str2 = str1.toLowerCase();
         // 0을 대문자 A로 변경 - 이전 과정에서 소문자로 변환된 "A"도 다시 "A"로 변환
-        answer = str2.replace("-", "A").replace("a", "A");
+        answer = str2.replace("0", "A").replace("a", "A");
         
         // 정답 리턴
         return answer;
