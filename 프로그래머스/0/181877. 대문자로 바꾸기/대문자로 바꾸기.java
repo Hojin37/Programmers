120ee75e954dec79274948b74d4fc557270c74ff
hozn_e <hwlsdl37@gmail.com>
2024-04-30 22:59:59 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        // 정답 선언 및 초기화
+        String answer = "";
+        // toUpperCase()를 이용해 대문자로 바꾸기
+        answer = myString.toUpperCase();
+        
+        // 정답 리턴
+        return answer;
+    }
+}
