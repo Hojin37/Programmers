9de7a31fa66caba7bb3302aa4a63554b234cf406
hozn_e <hwlsdl37@gmail.com>
2024-04-30 22:59:03 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        // 정답 선언 및 초기화
+        String answer = "";
+        // toLowerCase()를 사용해 소문자로 바꾸기
+        answer = myString.toLowerCase();
+        
+        // 정답 리턴
+        return answer;
+    }
+}
