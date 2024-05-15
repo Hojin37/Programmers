2a88162338b6516e2acc5fb94de51b9c15076613
hozn_e <hwlsdl37@gmail.com>
2024-05-15 22:53:47 +0900

    Update 0 떼기.java
    주석달기 전에 커밋이 되어버려서
    주석추가

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -2,16 +2,19 @@ class Solution {
     public String solution(String n_str) {
         // 정답 선언 및 초기화
         String answer = "";
+        // 0이 아닌 문자가 나타나는 부분의 index를 선언 및 초기화
         int index = 0;
         // n_str을 char배열로 변환
         char[] arr = n_str.toCharArray();
-        // 앞부분의 "0"문자들을 제거하기 위한 for문
+        
+        // 앞부분부터 '0'이 아닌 문자가 나타나면 그 i를 index에 저장
         for(int i=0; i<arr.length; i++){
             if(arr[i] != '0'){
                 index = i;
                 break;
             }
         }   
+        // 저장된 index부터 answer에 추가
         for(int j=index; j<arr.length; j++){
             answer += arr[j];
         }
