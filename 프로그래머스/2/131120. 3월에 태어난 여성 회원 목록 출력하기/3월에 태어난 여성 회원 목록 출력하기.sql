71ca38e05c6912594cce3ae62a2b22e231f9ad1f
hozn_e <hwlsdl37@gmail.com>
2024-07-11 22:56:14 +0900

    add MySQL

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -10,7 +10,25 @@ WHERE
     tlno IS NOT NULL
     AND
     TO_CHAR(date_of_birth, 'MM') = '03'
-    AND gender = 'W'
+    AND 
+    gender = 'W'
 ORDER BY
     member_id ASC
 ;
+
+-- MySQL
+SELECT 
+    MEMBER_ID, 
+    MEMBER_NAME, 
+    GENDER, 
+    DATE_FORMAT(DATE_OF_BIRTH, "%Y-%m-%d") AS DATE_OF_BIRTH
+FROM 
+    MEMBER_PROFILE
+WHERE 
+    GENDER LIKE 'W'
+    AND 
+    DATE_OF_BIRTH LIKE "%-03-%"
+    AND 
+    TLNO IS NOT NULL
+ORDER BY 
+    MEMBER_ID ASC;
