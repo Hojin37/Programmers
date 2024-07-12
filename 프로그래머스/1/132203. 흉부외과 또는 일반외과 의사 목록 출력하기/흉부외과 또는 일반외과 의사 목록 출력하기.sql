c47e3657b602fb97f1a2ab5c9f08535c1f7e9a34
hozn_e <hwlsdl37@gmail.com>
2024-07-12 22:30:31 +0900

    add MySQL

--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -14,3 +14,20 @@ ORDER BY
     hire_ymd DESC,
     dr_name ASC
 ;
+
+-- MySQL
+SELECT 
+    DR_NAME, 
+    DR_ID, 
+    MCDP_CD, 
+    DATE_FORMAT(HIRE_YMD, "%Y-%m-%d")
+FROM 
+    DOCTOR
+WHERE 
+    MCDP_CD LIKE "CS" 
+    OR 
+    MCDP_CD LIKE "GS"
+ORDER BY 
+    HIRE_YMD DESC, 
+    DR_NAME ASC
+;
