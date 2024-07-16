116f81a7141b5f387fcc92bd56bc8208bf590401
hozn_e <hwlsdl37@gmail.com>
2024-07-16 22:52:10 +0900

    Oracle & MySQL 조건에 부합하는 중고거래 댓글 조회하기.sql

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,23 +1,43 @@
--- 코드를 입력하세요
+-- Oracle
+SELECT
+    B.title,
+    B.board_id,
+    R.reply_id,
+    R.writer_id,
+    R.contents,
+    TO_CHAR(R.created_date, 'YYYY-MM-DD') AS created_date
+FROM
+    used_goods_board B,
+    used_goods_reply R
+WHERE
+    B.board_id = R.board_id
+    AND
+    TO_CHAR(B.created_date, 'YYYY-MM') = '2022-10'
+ORDER BY
+    R.created_date ASC,
+    B.title ASC
+;
+
+-- MySQL
 SELECT
     B.TITLE,
     B.BOARD_ID,
     R.REPLY_ID,
     R.WRITER_ID,
     R.CONTENTS,
-    TO_CHAR(R.CREATED_DATE, 'YYYY-MM-DD') AS CREATED_DATE
+    DATE_FORMAT(R.CREATED_DATE, "%Y-%m-%d") AS CREATED_DATE
 FROM
-    used_goods_board B,
-    used_goods_reply R
+    USED_GOODS_BOARD B
+    JOIN
+        USED_GOODS_REPLY R
+        ON B.BOARD_ID = R.BOARD_ID
 WHERE
-    B.BOARD_ID = R.BOARD_ID
-    AND
-    TO_CHAR(B.created_date, 'YYYY-MM') = '2022-10'
+    B.CREATED_DATE
+    LIKE "2022-10%"
 ORDER BY
-    R.created_date asc,
-    B.title asc
+    R.CREATED_DATE ASC,
+    B.TITLE ASC
 ;
-    
     
     
     
