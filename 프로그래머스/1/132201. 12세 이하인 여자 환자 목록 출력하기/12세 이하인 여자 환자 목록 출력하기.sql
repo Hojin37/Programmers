9d9d89c0cf7560e3e30c0b3f0647133ea5fdef67
hozn_e <hwlsdl37@gmail.com>
2024-07-17 23:04:50 +0900

    Oracle & MySQL 12세 이하인 여자 환자 목록 출력하기.sql

--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -14,3 +14,21 @@ ORDER BY
     age DESC,
     pt_name ASC
 ;
+
+-- MySQL
+SELECT
+    PT_NAME,
+    PT_NO,
+    GEND_CD,
+    AGE,
+    IFNULL(TLNO, 'NONE') AS TLNO
+FROM
+    PATIENT
+WHERE
+    GEND_CD LIKE 'W'
+HAVING
+    AGE < 13
+ORDER BY
+    AGE DESC,
+    PT_NAME ASC
+;
