890b981a49f31c2d435d88c4a4540280d4376aff
hozn_e <hwlsdl37@gmail.com>
2024-07-17 23:01:36 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,6 +1,16 @@
--- 코드를 입력하세요
-SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, "NONE") AS TLNO
-FROM PATIENT
-WHERE GEND_CD LIKE 'W'
-HAVING AGE < 13
-ORDER BY AGE DESC, PT_NAME ASC;
+-- Oracle
+SELECT
+    pt_name,
+    pt_no,
+    gend_cd,
+    age,
+    NVL(tlno, 'NONE') AS tlno
+FROM
+    patient
+WHERE
+    gend_cd = 'W'
+    AND age < 13
+ORDER BY
+    age DESC,
+    pt_name ASC
+;
