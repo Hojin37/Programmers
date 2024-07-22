301a6effa5d9b0fd8063585be04e2c07d9f19cff
hozn_e <hwlsdl37@gmail.com>
2024-07-22 22:40:01 +0900

    Oracle & MySQL 어린 동물 찾기.sql

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -7,5 +7,17 @@ FROM
 WHERE
     intake_condition != 'Aged'
 ORDER BY
-    animal_id
+    animal_id ASC
+;
+
+-- MySQL
+SELECT
+    ANIMAL_ID,
+    NAME
+FROM
+    ANIMAL_INS
+WHERE
+    INTAKE_CONDITION NOT LIKE 'Aged'
+ORDER BY
+    ANIMAL_ID ASC
 ;
