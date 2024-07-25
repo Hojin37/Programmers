d6c4f175d58fdf3722a8f0f464497e58c936eb10
hozn_e <hwlsdl37@gmail.com>
2024-07-25 21:36:09 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,2 +1,10 @@
--- 코드를 입력하세요
-SELECT COUNT(*) AS USERS FROM USER_INFO WHERE AGE BETWEEN "20" AND "29" AND JOINED LIKE "2021%";
+-- Oracle
+SELECT
+    COUNT(*) AS users
+FROM
+    user_info
+WHERE
+    age > 19 
+    AND age < 30
+    AND TO_CHAR(joined, 'YYYY') = '2021'
+;
