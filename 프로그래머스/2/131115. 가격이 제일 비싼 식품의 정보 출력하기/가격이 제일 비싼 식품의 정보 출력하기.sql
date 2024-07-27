b9301d950652427c4a114298e36b86128002fe31
hozn_e <hwlsdl37@gmail.com>
2024-07-27 22:24:04 +0900

    Oracle & MySQL 가격이 제일 비싼 식품의 정보 출력하기.sql

--- 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
+++ 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -1,14 +1,20 @@
 -- Oracle
 SELECT
-    product_id,
-    product_name,
-    product_cd,
-    category,
-    price
+    *
 FROM
     (SELECT *
      FROM food_product
      ORDER BY price DESC)
 WHERE
     ROWNUM = 1
+;
+
+-- MySQL
+SELECT
+    *
+FROM
+    FOOD_PRODUCT
+ORDER BY
+    PRICE DESC
+LIMIT 1
 ;
