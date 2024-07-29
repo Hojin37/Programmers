9e6a7bc26908a607ab37961894c1dea2e1f2b967
hozn_e <hwlsdl37@gmail.com>
2024-07-29 22:49:34 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,5 +1,10 @@
--- 코드를 입력하세요
-SELECT DATETIME AS '시간'
-FROM ANIMAL_INS
-ORDER BY DATETIME DESC
-LIMIT 1;
+-- Oracle
+SELECT
+    datetime AS 시간
+FROM
+    (SELECT *
+     FROM animal_ins
+     ORDER BY datetime DESC)
+WHERE
+    ROWNUM = 1
+;
