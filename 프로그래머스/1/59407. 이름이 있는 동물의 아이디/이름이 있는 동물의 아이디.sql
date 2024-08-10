22ef14f39a55f7013ba4bd0a30307753f8c6528a
hozn_e <hwlsdl37@gmail.com>
2024-08-10 21:14:34 +0900

    Oracle & MySQL 이름이 있는 동물의 아이디.sql

--- 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
+++ 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
@@ -1,2 +1,17 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID FROM ANIMAL_INS WHERE NAME IS NOT NULL;
+-- Oracle
+SELECT
+		animal_id
+FROM
+		animal_ins
+WHERE
+		name IS NOT NULL
+;
+
+-- MySQL
+SELECT 
+		ANIMAL_ID 
+FROM 
+		ANIMAL_INS 
+WHERE 
+		NAME IS NOT NULL
+;
