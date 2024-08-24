fc209f7785ff88d45eb253f05dae44ff08fc7699
hozn_e <hwlsdl37@gmail.com>
2024-08-24 23:07:04 +0900

    Oracle & MySQL NULL 처리하기.sql

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -8,4 +8,14 @@ FROM
 ORDER BY
     animal_id ASC
 ;
-    
+
+-- MySQL
+SELECT
+    ANIMAL_TYPE,
+    IFNULL(NAME, 'No name') AS NAME,
+    SEX_UPON_INTAKE
+FROM
+    ANIMAL_INS
+ORDER BY
+    ANIMAL_ID ASC
+;
