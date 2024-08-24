51d669e60ea9c4864fd474122e96abda16a7c2e1
hozn_e <hwlsdl37@gmail.com>
2024-08-24 23:04:52 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,3 +1,11 @@
--- 코드를 입력하세요
-SELECT ANIMAL_TYPE, IFNULL(NAME, "No name") AS NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS;
+-- Oracle
+SELECT
+    animal_type,
+    CASE WHEN name IS NULL THEN 'No name' ELSE name END AS name,
+    sex_upon_intake
+FROM
+    animal_ins
+ORDER BY
+    animal_id ASC
+;
+    
