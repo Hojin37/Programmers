470bc88a1df8e1131c755c542e516c049039c1b2
hozn_e <hwlsdl37@gmail.com>
2024-08-26 22:38:55 +0900

    Oracle & MySQL 고양이와 개는 몇 마리 있을까.sql

--- 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -1,11 +1,23 @@
 -- Oracle
 SELECT
     animal_type,
-    count(*)
+    count(*) as count
 FROM
     animal_ins
 GROUP BY
     animal_type
 ORDER BY
     animal_type
+;
+
+-- MySQL
+SELECT
+    ANIMAL_TYPE,
+    COUNT(*) AS COUNT
+FROM
+    ANIMAL_INS
+GROUP BY
+    ANIMAL_TYPE
+ORDER BY
+    ANIMAL_TYPE
 ;
