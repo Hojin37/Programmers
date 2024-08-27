1b9445ce76464abaf9cbf8261f68e0b926cc5c38
hozn_e <hwlsdl37@gmail.com>
2024-08-27 23:13:02 +0900

    Oracle & MySQL 상품 별 오프라인 매출 구하기.sql

--- 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
+++ 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -13,3 +13,19 @@ order by
     sales desc, 
     p.product_code asc
 ;
+
+-- MySQL
+SELECT 
+    P.PRODUCT_CODE,
+    (SUM(O.SALES_AMOUNT) * P.PRICE) AS SALES
+FROM 
+    PRODUCT P
+JOIN 
+    OFFLINE_SALE O ON P.PRODUCT_ID = O.PRODUCT_ID
+GROUP BY 
+    P.PRODUCT_CODE, 
+    P.PRICE
+ORDER BY 
+    SALES DESC, 
+    P.PRODUCT_CODE ASC
+;
