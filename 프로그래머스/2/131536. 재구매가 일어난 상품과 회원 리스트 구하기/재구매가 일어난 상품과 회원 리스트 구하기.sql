968667a595acf4a590d6f6b7e8d638aadb159de6
hozn_e <hwlsdl37@gmail.com>
2024-08-31 22:14:00 +0900

    Oracle & MySQL 재구매가 일어난 상품과 회원 리스트 구하기.sql

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -13,3 +13,19 @@ order by
     user_id asc,
     product_id desc
 ;
+
+-- MySQL
+SELECT 
+    USER_ID, 
+    PRODUCT_ID
+FROM 
+    ONLINE_SALE
+GROUP BY 
+    USER_ID, 
+    PRODUCT_ID
+HAVING 
+    COUNT(*) > 1
+ORDER BY 
+    USER_ID ASC, 
+    PRODUCT_ID DESC
+;
