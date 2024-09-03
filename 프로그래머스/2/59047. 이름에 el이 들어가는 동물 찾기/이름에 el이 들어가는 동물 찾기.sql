106bf677a3179b9cc0aaeb0be27e4f2c55bf35cb
hozn_e <hwlsdl37@gmail.com>
2024-09-03 23:00:26 +0900

    Oracle & MySQL 이름에 el이 들어가는 동물 찾기.sql

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -6,7 +6,20 @@ from
     animal_ins
 where
     animal_type = 'Dog'
-    and upper(name) like '%EL%'
+    and upper(name) like '%EL%' -- Oracle에서는 대소문자를 구분하기 때문에 대문자로 바꾼 후 검색
 order by
     name asc
+;
+
+-- MySQL
+SELECT
+    ANIMAL_ID,
+    NAME
+FROM
+    ANIMAL_INS
+WHERE
+    ANIMAL_TYPE LIKE 'DOG'
+    AND NAME LIKE "%el%" -- MySQL의 Like는 대소문자를 구분하지 않음
+ORDER BY
+    NAME ASC
 ;
