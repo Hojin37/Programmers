be5fbb99e8a8092894113185586ca93aa331a762
hozn_e <hwlsdl37@gmail.com>
2024-09-04 22:06:15 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,11 +1,18 @@
--- 코드를 입력하세요
-SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
-CASE 
-    WHEN STATUS = 'DONE' THEN '거래완료'
-    WHEN STATUS = 'SALE' THEN '판매중'
-    WHEN STATUS = 'RESERVED' THEN '예약중'
-    ELSE STATUS 
-END AS STATUS
-FROM USED_GOODS_BOARD
-WHERE CREATED_DATE LIKE "2022-10-05"
-ORDER BY BOARD_ID DESC;
+-- Oracle
+select
+    board_id,
+    writer_id,
+    title,
+    price,
+    case 
+        when status = 'DONE' then '거래완료'
+        when status = 'RESERVED' then '예약중'
+        else '판매중' end
+    as "status"
+from
+    used_goods_board
+where
+    created_date =  to_date('2022-10-05', 'YYYY-MM-DD') 
+order by
+    board_id desc
+;
