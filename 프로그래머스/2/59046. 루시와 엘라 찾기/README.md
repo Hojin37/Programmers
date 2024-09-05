4ee8abc5ad80512cccdb2b8b8dade3088704bc55
hozn_e <hwlsdl37@gmail.com>
2024-09-05 21:47:40 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/README.md
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 루시와 엘라 찾기 - 59046 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59046) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59046?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 06일 22:52:44
+2024년 09월 05일 21:47:38
 
 ### 문제 설명
 
--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,10 +1,17 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS
-WHERE NAME LIKE "Lucy" 
-OR NAME LIKE "Ella" 
-OR NAME LIKE "Pickle" 
-OR NAME LIKE "Rogan"
-OR NAME LIKE "Sabrina"
-OR NAME LIKE "Mitty"
-ORDER BY ANIMAL_ID;
+-- Oracle
+select
+    animal_id,
+    name,
+    sex_upon_intake
+from
+    animal_ins
+where
+    name = 'Lucy'
+    or name = 'Ella'
+    or name = 'Pickle'
+    or name = 'Rogan'
+    or name = 'Sabrina'
+    or name = 'Mitty'
+order by
+    animal_id asc
+;
