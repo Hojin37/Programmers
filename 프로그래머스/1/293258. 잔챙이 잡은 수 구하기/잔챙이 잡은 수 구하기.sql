7d99c2545e26c0ae7ef38ecc04b8b3bc871f9b1f
hozn_e <hwlsdl37@gmail.com>
2024-09-06 23:37:05 +0900

    Oracle & MySQL 잔챙이 잡은 수 구하기.sql

--- 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
+++ 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
@@ -1,4 +1,17 @@
--- 코드를 작성해주세요
-SELECT COUNT(*) AS FISH_COUNT
-FROM FISH_INFO
-WHERE LENGTH IS NULL;
+-- Oracle
+select
+	count(*) as fish_count
+from
+	fish_info
+where
+	length is null
+;
+
+-- MySQL
+SELECT 
+	COUNT(*) AS FISH_COUNT
+FROM 
+	FISH_INFO
+WHERE 
+	LENGTH IS NULL
+;
