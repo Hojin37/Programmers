2912ee927c1e2269b3fffb8f4b399a34448b82d5
hozn_e <hwlsdl37@gmail.com>
2024-09-07 23:07:01 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/README.md
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 중성화 여부 파악하기 - 59409 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59409) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59409?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 10일 22:58:23
+2024년 09월 07일 23:06:55
 
 ### 문제 설명
 
--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,3 +1,14 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, IF(SEX_UPON_INTAKE LIKE "%Neutered%" OR SEX_UPON_INTAKE LIKE "Spayed%", "O", "X") AS 중성화
-FROM ANIMAL_INS;
+-- Oracle
+select
+    animal_id,
+    name,
+    case 
+        when sex_upon_intake like '%Neutered%' then 'O'
+        when sex_upon_intake like '%Spayed%' then 'O'
+        else 'X' 
+    end as sex_upon_intake
+from
+    animal_ins
+order by
+    animal_id asc
+;
