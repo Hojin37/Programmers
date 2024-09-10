12fe14458a94c0d70260cb46bb11a061300fde91
hozn_e <hwlsdl37@gmail.com>
2024-09-10 21:49:33 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/README.md
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/README.md
@@ -1,6 +1,6 @@
 # [level 2] 성분으로 구분한 아이스크림 총 주문량 - 133026 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133026) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133026?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 08일 23:27:09
+2024년 09월 10일 21:49:28
 
 ### 문제 설명
 
--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -1,6 +1,14 @@
--- 코드를 입력하세요
-SELECT I.INGREDIENT_TYPE, SUM(F.TOTAL_ORDER) AS TOTAL_ORDER
-FROM ICECREAM_INFO I
-JOIN FIRST_HALF F ON I.FLAVOR = F.FLAVOR
-GROUP BY I.INGREDIENT_TYPE
-ORDER BY SUM(F.TOTAL_ORDER) ASC;
+-- Oracle
+select
+    i.ingredient_type,
+    sum(f.total_order) as total_order
+from    
+    first_half f,
+    icecream_info i
+where
+    f.flavor = i.flavor
+group by
+    i.ingredient_type
+order by
+    sum(f.total_order) asc
+;
