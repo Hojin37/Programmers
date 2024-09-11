bdd12b27490777ddb4072808ae063e313a59f39e
hozn_e <hwlsdl37@gmail.com>
2024-09-11 22:20:40 +0900

    Oracle & MySQL 가격대 별 상품 개수 구하기.sql

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -9,3 +9,15 @@ group by
 order by
     price_group
 ;
+
+-- MySQL
+SELECT 
+    FLOOR(price / 10000) * 10000 AS PRICE_GROUP,
+    COUNT(*) AS PRODUCTS
+FROM 
+    PRODUCT
+GROUP BY 
+    PRICE_GROUP
+ORDER BY 
+    PRICE_GROUP
+;
