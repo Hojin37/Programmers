f037d6dd9bd95a67182e129e80c579324e700ae8
hozn_e <hwlsdl37@gmail.com>
2024-09-11 22:18:00 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/README.md
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 가격대 별 상품 개수 구하기 - 131530 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131530) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131530?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 19일 22:19:43
+2024년 09월 11일 22:17:52
 
 ### 문제 설명
 
--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,4 +1,11 @@
-SELECT FLOOR(price / 10000) * 10000 AS PRICE_GROUP, COUNT(*) AS PRODUCTS
-FROM PRODUCT
-GROUP BY FLOOR(price / 10000) * 10000
-ORDER BY PRICE_GROUP;
+-- Oracle
+select
+    floor(price/10000) * 10000 as price_group,
+    count(*) as products
+from
+    product
+group by
+    floor(price/10000) * 10000
+order by
+    price_group
+;
