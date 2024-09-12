921b6ca7342d8185ae66dd84e5b077434dac8f68
hozn_e <hwlsdl37@gmail.com>
2024-09-12 22:51:58 +0900

    Oracle & MySQL 진료과별 총 예약 횟수 출력하기.sql

--- 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++ 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -12,3 +12,18 @@ order by
     count(mcdp_cd) asc,
     mcdp_cd asc
 ;
+
+-- MySQL
+SELECT 
+    MCDP_CD AS "진료과코드", 
+    COUNT(MCDP_CD) AS "5월예약건수"
+FROM 
+    APPOINTMENT
+WHERE 
+    APNT_YMD LIKE "2022-05%"
+GROUP BY 
+    MCDP_CD
+ORDER BY 
+    COUNT(MCDP_CD) ASC, 
+    MCDP_CD ASC
+;
