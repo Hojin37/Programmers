3bcaac550a058ea1d8d8f3b51a8f511c7f61e623
hozn_e <hwlsdl37@gmail.com>
2024-09-13 22:30:32 +0900

    [level 2] Title: 동명 동물 수 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
@@ -0,0 +1,15 @@
+-- Oracle
+select
+    name,
+    count(name) as count
+from
+    animal_ins
+where
+    name is not null
+group by
+    name
+having
+    count(name) >= 2
+order by
+    name asc
+;
