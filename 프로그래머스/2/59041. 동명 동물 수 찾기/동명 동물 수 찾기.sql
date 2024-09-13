8e712e23ac6ebdce315dedb20c7d6e7c9722e4bb
hozn_e <hwlsdl37@gmail.com>
2024-09-13 22:31:52 +0900

    Oracle & MySQL 동명 동물 수 찾기.sql

--- 프로그래머스/2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
+++ 프로그래머스/2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
@@ -13,3 +13,19 @@ having
 order by
     name asc
 ;
+
+-- MySQL
+SELECT 
+    NAME, 
+    COUNT(NAME) AS COUNT
+FROM 
+    ANIMAL_INS 
+WHERE 
+    NAME IS NOT NULL
+GROUP BY 
+    NAME
+HAVING 
+    COUNT > 1
+ORDER 
+    BY NAME
+;
