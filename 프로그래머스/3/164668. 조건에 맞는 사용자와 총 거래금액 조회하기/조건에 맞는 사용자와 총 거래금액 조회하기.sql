725278930b46a824298a234ab9c077d5f6f95e6e
hozn_e <hwlsdl37@gmail.com>
2024-09-14 22:58:15 +0900

    Oracle & MySQL 조건에 맞는 사용자와 총 거래금액 조회하기.sql

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -17,3 +17,23 @@ having
 order by
     total_sales asc
 ;
+
+-- MySQL
+SELECT 
+    U.USER_ID, 
+    U.NICKNAME, 
+    SUM(B.PRICE) AS TOTAL_SALES
+FROM 
+    USED_GOODS_BOARD B
+JOIN 
+    USED_GOODS_USER U 
+    ON B.WRITER_ID = U.USER_ID
+WHERE 
+    B.STATUS = 'DONE'
+GROUP BY 
+    U.USER_ID
+HAVING
+    SUM(B.PRICE) >= 700000
+ORDER BY
+    TOTAL_SALES ASC
+;
