80816637c8be2d2079c672989466498546df9118
hozn_e <hwlsdl37@gmail.com>
2024-09-16 22:56:55 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,15 @@
--- 코드를 입력하세요
-SELECT O.ANIMAL_ID, O.NAME
-FROM ANIMAL_OUTS O
-LEFT JOIN ANIMAL_INS I ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE I.ANIMAL_ID IS NULL
-ORDER BY I.ANIMAL_ID ASC;
+-- Oracle
+select
+    o.animal_id,
+    o.name
+from
+    animal_outs o
+left outer join
+    animal_ins i
+on
+    o.animal_id = i.animal_id
+where
+    i.animal_id is null
+order by
+    animal_id asc
+;
