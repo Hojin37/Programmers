0de9f54673b7557e2db71d0b15cba3a2e3c20703
hozn_e <hwlsdl37@gmail.com>
2024-11-06 22:08:44 +0900

    Oracle & MySQL 식품분류별 가장 비싼 식품의 정보 조회하기.sql

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -12,10 +12,28 @@ join
      where category in ('과자', '국', '김치', '식용유')
      group by category
     ) max_p
-on 
-    p.category = max_p.category 
-and 
-    p.price = max_p.max_price
+    on p.category = max_p.category 
+    and p.price = max_p.max_price
 order by
     p.price desc
+;
+
+-- MySQL
+SELECT
+    P.CATEGORY, 
+    P.PRICE, 
+    P.PRODUCT_NAME
+FROM
+    FOOD_PRODUCT P
+JOIN
+    (SELECT CATEGORY, 
+     MAX(PRICE) AS MAX_PRICE
+     FROM FOOD_PRODUCT
+     WHERE CATEGORY IN ('과자', '국', '김치', '식용유')
+     GROUP BY CATEGORY) 
+     AS MAX_P
+    ON P.CATEGORY = MAX_P.CATEGORY 
+    AND P.PRICE = MAX_P.MAX_PRICE
+ORDER BY
+    P.PRICE DESC
 ;
