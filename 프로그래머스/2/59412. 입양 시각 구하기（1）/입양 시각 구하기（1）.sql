f5e2db4c747dc8e09c45e983e8a903ec8cdae51e
hozn_e <hwlsdl37@gmail.com>
2024-11-07 22:36:57 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,13 @@
--- 코드를 입력하세요
-SELECT DATE_FORMAT(DATETIME, "%H") AS HOUR, COUNT(*) AS COUNT
-FROM ANIMAL_OUTS
-WHERE DATE_FORMAT(DATETIME, "%H") BETWEEN 09 AND 19
-GROUP BY DATE_FORMAT(DATETIME, "%H")
-ORDER BY DATE_FORMAT(DATETIME, "%H");
+-- Oracle
+select 
+    to_number(to_char(datetime, 'hh24')) as hour,
+    count(*) as count
+from
+    animal_outs
+where
+    to_char(datetime, 'hh24') between '09' and '19'
+group by
+    to_number(to_char(datetime, 'hh24'))
+order by
+    to_number(to_char(datetime, 'hh24'))
+;
