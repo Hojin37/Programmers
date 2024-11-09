930f1b92e0d9557b98ba7c50012e6bdf721ed824
hozn_e <hwlsdl37@gmail.com>
2024-11-09 23:17:28 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,20 +1,17 @@
-SELECT 
-    I.FOOD_TYPE,
-    I.REST_ID,
-    I.REST_NAME,
-    I.FAVORITES
-FROM 
-    REST_INFO I
-JOIN
-    (SELECT 
-        FOOD_TYPE, 
-        MAX(FAVORITES) AS MAX_FAVOR
-    FROM 
-        REST_INFO
-    GROUP BY 
-        FOOD_TYPE) 
-    AS F
-    ON I.FOOD_TYPE = F.FOOD_TYPE
-    AND I.FAVORITES = F.MAX_FAVOR
-ORDER BY 
-    I.FOOD_TYPE DESC;
+-- Oracle
+select 
+    i.food_type,
+    i.rest_id,
+    i.rest_name,
+    i.favorites
+from 
+    rest_info i
+where 
+    i.favorites = (
+        select max(favorites)
+        from rest_info
+        where food_type = i.food_type
+        )
+order by 
+    i.food_type desc
+;
