a84b3fadc4447311f5420d19f6f2dcf70420b66a
hozn_e <hwlsdl37@gmail.com>
2024-11-09 23:19:43 +0900

    Oracle & MySQL 즐겨찾기가 가장 많은 식당 정보 출력하기.sql

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -15,3 +15,22 @@ where
 order by 
     i.food_type desc
 ;
+
+-- MySQL
+SELECT 
+    I.FOOD_TYPE,
+    I.REST_ID,
+    I.REST_NAME,
+    I.FAVORITES
+FROM 
+    REST_INFO I
+JOIN
+    (SELECT FOOD_TYPE, 
+        MAX(FAVORITES) AS MAX_FAVOR
+    FROM REST_INFO
+    GROUP BY FOOD_TYPE) AS F
+    ON I.FOOD_TYPE = F.FOOD_TYPE
+    AND I.FAVORITES = F.MAX_FAVOR
+ORDER BY 
+    I.FOOD_TYPE DESC
+;
