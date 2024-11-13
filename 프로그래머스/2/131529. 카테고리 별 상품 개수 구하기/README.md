e2c0b30dbe56ba1b228a6474518422e94559185e
hozn_e <hwlsdl37@gmail.com>
2024-11-13 22:31:21 +0900

    [level 2] Title: 카테고리 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/README.md
+++ 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 카테고리 별 상품 개수 구하기 - 131529 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131529) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131529?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 08일 23:40:40
+2024년 11월 13일 22:31:16
 
 ### 문제 설명
 
--- 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
@@ -1,5 +1,11 @@
 -- 코드를 입력하세요
-SELECT LEFT(PRODUCT_CODE, 2) AS CATEGORY, COUNT(PRODUCT_CODE) AS PRODUCTS
-FROM PRODUCT
-GROUP BY LEFT(PRODUCT_CODE, 2)
-ORDER BY LEFT(PRODUCT_CODE, 2);
+select
+    substr(product_code, 1, 2) as category, 
+    count(product_code) as products
+from
+    product
+group by
+    substr(product_code, 1, 2)
+order by
+    substr(product_code, 1, 2) asc
+;
