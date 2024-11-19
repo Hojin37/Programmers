196229d5c795147866accf1eb176daa93eb2155c
hozn_e <hwlsdl37@gmail.com>
2024-11-19 22:58:24 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,13 @@
--- 코드를 입력하세요
-SELECT O.ANIMAL_ID, I.NAME
-FROM ANIMAL_OUTS O
-JOIN ANIMAL_INS I ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE O.DATETIME < I.DATETIME
-ORDER BY I.DATETIME ASC;
+-- Oracle
+select
+    I.animal_id,
+    I.name
+from
+    animal_ins I,
+    animal_outs O
+where
+    I.animal_id = O.animal_id
+    and I.datetime > O.datetime
+order by
+    I.datetime asc
+;
