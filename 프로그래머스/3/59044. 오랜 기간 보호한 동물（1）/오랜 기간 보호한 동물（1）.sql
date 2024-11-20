436580ccdde2b22cbe41f89aeb9f2e900b814b52
hozn_e <hwlsdl37@gmail.com>
2024-11-20 22:26:16 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,15 @@
--- 코드를 입력하세요
-SELECT I.NAME, I.DATETIME
-FROM ANIMAL_INS I
-LEFT JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE O.DATETIME IS NULL
-ORDER BY I.DATETIME ASC
-LIMIT 3;
+-- Oracle
+select
+    I.name,
+    I.datetime
+from
+    animal_ins I
+left join
+    animal_outs O
+    on I.animal_id = O.animal_id
+where    
+    O.datetime is null
+order by
+    I.datetime asc
+fetch first 3 rows only
+;
