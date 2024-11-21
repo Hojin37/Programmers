33d2e30e48135f4e59c48376bdaeba8959ac3538
hozn_e <hwlsdl37@gmail.com>
2024-11-21 23:01:35 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
@@ -1,6 +1,6 @@
 # [level 4] 보호소에서 중성화한 동물 - 59045 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 13일 22:52:22
+2024년 11월 21일 23:01:33
 
 ### 문제 설명
 
--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,8 +1,16 @@
--- 코드를 입력하세요
-SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
-FROM ANIMAL_INS I
-JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE I.SEX_UPON_INTAKE LIKE "Intact%"
-AND (O.SEX_UPON_OUTCOME LIKE "Spayed%"
-OR O.SEX_UPON_OUTCOME LIKE "Neutered%")
-ORDER BY I.ANIMAL_ID ASC;
+-- Oracle
+select
+    I.animal_id,
+    I.animal_type,
+    I.name
+from
+    animal_ins I,
+    animal_outs O
+where
+    I.animal_id = O.animal_id
+    and I.sex_upon_intake like 'Intact%'
+    and (O.sex_upon_outcome like 'Spayed%'
+        or O.sex_upon_outcome like 'Neutered%')
+order by
+    1 asc
+;
