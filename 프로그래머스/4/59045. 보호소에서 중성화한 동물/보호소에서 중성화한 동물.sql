b8255ee2f2dca50819f4274c996d4ee88df093cf
hozn_e <hwlsdl37@gmail.com>
2024-11-21 23:03:42 +0900

    Oracle & MySQL 보호소에서 중성화한 동물.sql

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -14,3 +14,21 @@ where
 order by
     1 asc
 ;
+
+-- MySQL
+SELECT 
+    I.ANIMAL_ID, 
+    I.ANIMAL_TYPE, 
+    I.NAME
+FROM 
+    ANIMAL_INS I
+JOIN 
+    ANIMAL_OUTS O 
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE 
+    I.SEX_UPON_INTAKE LIKE "Intact%"
+    AND (O.SEX_UPON_OUTCOME LIKE "Spayed%"
+        OR O.SEX_UPON_OUTCOME LIKE "Neutered%")
+ORDER BY 
+    I.ANIMAL_ID ASC
+;
