b99acd374920ba03e759c279f4bc19b368ceddbb
hozn_e <hwlsdl37@gmail.com>
2024-11-23 23:04:20 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,24 +1,19 @@
--- 코드를 입력하세요
-SELECT
-    CONCAT("/home/grep/src/",
-           F.BOARD_ID,
-           "/",
-           F.FILE_ID,
-           F.FILE_NAME,
-           F.FILE_EXT) AS FILE_PATH
-FROM
-    USED_GOODS_BOARD B
-    JOIN
-        USED_GOODS_FILE F
-        ON B.BOARD_ID = F.BOARD_ID
-WHERE 
-    B.VIEWS = (SELECT MAX(VIEWS) 
-            FROM USED_GOODS_BOARD)
-GROUP BY
-    B.BOARD_ID,
-    F.BOARD_ID,
-    F.FILE_ID,
-    F.FILE_NAME,
-    F.FILE_EXT
-ORDER BY
-    F.FILE_ID DESC;
+-- Oracle
+select
+    '/home/grep/src/' || F.board_id || '/' || F.file_id || F.file_name || F.file_ext as "FILE_PATH"
+from
+    used_goods_board B
+join used_goods_file F
+    on B.board_id = F.board_id
+where 
+    B.views = (select max(views) 
+               from used_goods_board)
+group by
+    B.board_id,
+    F.board_id,
+    F.file_id,
+    F.file_name,
+    F.file_ext
+order by
+    F.file_id desc
+;
