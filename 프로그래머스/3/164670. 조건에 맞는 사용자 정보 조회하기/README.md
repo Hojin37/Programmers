bd5417b9f8363a26742e78e91ca56f6876943edf
hozn_e <hwlsdl37@gmail.com>
2024-11-29 22:51:58 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 조건에 맞는 사용자 정보 조회하기 - 164670 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 29일 23:12:48
+2024년 11월 29일 22:51:55
 
 ### 문제 설명
 
--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,22 +1,23 @@
--- 코드를 입력하세요
-SELECT
-    U.USER_ID,
-    U.NICKNAME,
-    CONCAT(U.CITY, ' ', U.STREET_ADDRESS1, ' ', U.STREET_ADDRESS2) AS '전체주소',
-    CONCAT(SUBSTRING(TLNO, 1, 3), '-', SUBSTRING(TLNO, 4, 4), '-', SUBSTRING(TLNO, 8, 4)) AS '전화번호'
-FROM 
-    USED_GOODS_USER U
-JOIN
-    USED_GOODS_BOARD B
-    ON U.USER_ID = B.WRITER_ID
-GROUP BY
-    U.USER_ID,
-    U.NICKNAME,
-    U.CITY,
-    U.STREET_ADDRESS1,
-    U.STREET_ADDRESS2,
-    U.TLNO
-HAVING
-    COUNT(B.WRITER_ID) > 2
-ORDER BY
-    U.USER_ID DESC;
+-- Oracle
+select
+    u.user_id,
+    u.nickname,
+    u.city || ' ' || u.street_address1 || ' ' || u.street_address2 as 전체주소,
+    substr(u.tlno, 1, 3) || '-' || substr(u.tlno, 4, 4) || '-' || substr(u.tlno, 8, 4) as 전화번호
+from 
+    used_goods_user u
+join
+    used_goods_board b
+    on u.user_id = b.writer_id
+group by
+    u.user_id,
+    u.nickname,
+    u.city,
+    u.street_address1,
+    u.street_address2,
+    u.tlno
+having
+    count(b.writer_id) > 2
+order by
+    u.user_id desc
+;
