51a4c1f696612efcdd77cb7bc6d745abbd44fcfe
hozn_e <hwlsdl37@gmail.com>
2024-12-02 23:29:55 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,13 +1,15 @@
--- Oracle
-select 
-    flavor
-from 
-    (select flavor
-    from first_half
-    join july using(flavor)
-    group by flavor
-    order by sum(first_half.total_order) + sum(july.total_order) desc
-    )
-where 
-    rownum <= 3
+-- MySQL
+SELECT
+    F.FLAVOR
+FROM
+    FIRST_HALF F
+JOIN
+    JULY J
+    ON F.FLAVOR= J.FLAVOR
+GROUP BY
+    F.FLAVOR
+ORDER BY
+    SUM(F.TOTAL_ORDER) + SUM(J.SHIPMENT_ID) DESC
+LIMIT 3
 ;
+    
