e6fe59b8366da12e3b8cb48462ef77f7b9548272
hozn_e <hwlsdl37@gmail.com>
2024-12-02 23:30:55 +0900

    Oracle & MySQL 주문량이 많은 아이스크림들 조회하기.sql

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,3 +1,17 @@
+-- Oracle
+select 
+    flavor
+from 
+    (select flavor
+    from first_half
+    join july using(flavor)
+    group by flavor
+    order by sum(first_half.total_order) + sum(july.total_order) desc
+    )
+where 
+    rownum <= 3
+;
+
 -- MySQL
 SELECT
     F.FLAVOR
