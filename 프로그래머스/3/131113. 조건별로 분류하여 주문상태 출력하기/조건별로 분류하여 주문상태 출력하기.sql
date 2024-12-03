07c2140f6a5b7876c350e495d1be050c0a625d83
hozn_e <hwlsdl37@gmail.com>
2024-12-03 23:52:31 +0900

    Oracle & MySQL 조건별로 분류하여 주문상태 출력하기.sql

--- 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -19,3 +19,25 @@ from (
 order by 
     order_id asc
 ;
+
+-- MySQL
+SELECT 
+    ORDER_ID, 
+    PRODUCT_ID, 
+    OUT_DATE, 
+    CASE
+       WHEN OUT_DATE IS NULL THEN '출고미정'
+       WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+       ELSE '출고대기'
+   END AS 출고여부
+FROM (
+    SELECT 
+        ORDER_ID, 
+        PRODUCT_ID, 
+        DATE_FORMAT(OUT_DATE, "%Y-%m-%d") AS OUT_DATE
+    FROM 
+        FOOD_ORDER
+    ) AS sub
+ORDER BY 
+    ORDER_ID ASC
+;
