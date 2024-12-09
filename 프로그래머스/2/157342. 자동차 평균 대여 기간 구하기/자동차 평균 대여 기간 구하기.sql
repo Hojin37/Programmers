54dcfe2a63cc5c81ad2319695fc63d93bec679a4
hozn_e <hwlsdl37@gmail.com>
2024-12-09 22:23:36 +0900

    Oracle & MySQL 자동차 평균 대여 기간 구하기.sql

--- 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
+++ 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
@@ -1,3 +1,18 @@
+-- Oracle
+select 
+    car_id,
+    to_char(round(avg(end_date - start_date + 1), 1), 'FM9999990.0') as average_duration
+from 
+    car_rental_company_rental_history
+group by 
+    car_id
+having 
+    round(avg(end_date - start_date + 1), 1) >= 7
+order by 
+    round(avg(end_date - start_date + 1), 1) desc,
+    car_id desc
+;
+
 -- MySQL
 SELECT 
     CAR_ID,
